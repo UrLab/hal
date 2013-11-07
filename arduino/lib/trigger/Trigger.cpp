d18241c19d7f8ed11eee7c25df8440230339d2db
iTitou <titouanchristophe@gmail.com>
2013-11-07 12:06:09 +0100

    [enh] Add a minimum number of active physical states before Trigger becomes logically active

--- arduino/lib/trigger/Trigger.cpp
+++ arduino/lib/trigger/Trigger.cpp
@@ -1,24 +1,26 @@
 #include "Trigger.h"
 #include "Arduino.h"
 
-Trigger::Trigger(int pin, int idleState, unsigned long int timeout, char onActivate):
-_pin(pin), _idleState(idleState), _active(false), _onActivate(onActivate),
-_active_from(0), _timeout(timeout)
+Trigger::Trigger(int pin, int idleState, unsigned long int timeout, char onActivate, unsigned int activeAfter):
+_pin(pin), _idleState(idleState), _active(0), _onActivate(onActivate),
+_active_from(0), _timeout(timeout), _activeAfter(activeAfter)
 {
 	pinMode(_pin, INPUT);
 }
 
 void Trigger::activate(void)
 {
-	_active = true;
-	_active_from = millis();
-	if (_onActivate)
-		Serial.println(_onActivate);
+	_active++;
+	if (_active == _activeAfter){
+		_active_from = millis();
+		if (_onActivate)
+			Serial.println(_onActivate);
+	}
 }
 
 void Trigger::deactivate(void)
 {
-	_active = false;
+	_active = 0;
 }
 
 bool Trigger::isIdleState(void)
@@ -29,11 +31,12 @@ bool Trigger::isIdleState(void)
 bool Trigger::isActive(void)
 {
 	bool idle = isIdleState();
-	if (! _active && ! idle){
+	bool on = _active == _activeAfter;
+	if (! on && ! idle){
 		activate();
-	} else if (_active && idle) {
+	} else if (on && idle) {
 		if (millis()-_active_from >= _timeout)
 			deactivate();
 	}
-	return _active;
+	return _active == _activeAfter;
 }
--- arduino/lib/trigger/Trigger.h
+++ arduino/lib/trigger/Trigger.h
@@ -4,11 +4,11 @@
 class Trigger {
 	private:
 		int _pin, _idleState;
-		bool _active;
+		unsigned int _active, _activeAfter;
 		unsigned long int _active_from, _timeout;
 		char _onActivate;
 	public:
-		Trigger(int pin, int idleState, unsigned long int timeout=0, char onActivate=0);
+		Trigger(int pin, int idleState, unsigned long int timeout=0, char onActivate=0, unsigned int activeAfter=1);
 		void activate(void);
 		void deactivate(void);
 		bool isActive(void);
