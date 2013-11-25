98c47199518e64bebd794491a02f47840fa09170
iTitou <titouanchristophe@gmail.com>
2013-11-25 17:09:37 +0100

    [enh] Increase FPS to reduce latency

--- daemon.py
+++ daemon.py
@@ -168,7 +168,7 @@ class AmbianceDaemon(Ambianceduino):
 	def mainloop(self):
 		SAMPLES_PER_FRAME = 25
 		AUDIO_SAMPLE_RATE = 44100
-		FRAMES_PER_SECOND = 14
+		FRAMES_PER_SECOND = 25
 		SAMPLES_TO_DROP = (AUDIO_SAMPLE_RATE/FRAMES_PER_SECOND)-SAMPLES_PER_FRAME
 		with open("/tmp/mpd.fifo", "rb") as sound:
 			while True:
