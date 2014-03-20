8ce7a59b65a48286e77c2f89cfc9586037f1fb8a
iTitou <moiandme@gmail.com>
2014-03-20 20:45:21 +0100

    [ref] Use C !!!

--- daemon/driver.c
+++ daemon/driver.c
@@ -1,66 +1,74 @@
-#include "Ambianceduino.hpp"
-#include <iostream>
+#include "Ambianceduino.h"
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/file.h>
+
+#define __USE_BSD    /* For S_IFDIR */
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "utils.h"
 
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
 
-using namespace std;
+#define min(a, b) ((a) < (b)) ? (a) : (b)
 
-Ambianceduino * arduino;
+HAL arduino;
 
-typedef struct hal_file {
+typedef struct halfs_file {
    char * name;
    int mode;
    int (* size_callback)(const char *);
    int (* trunc_callback)(const char *);
    int (* read_callback)(const char *, char *, size_t, off_t);
    int (* write_callback)(const char *, const char *, size_t, off_t);
-} hal_file;
+} halfs_file;
 
-int default_size_callback(const char *){
+int default_size_callback(const char *path){
     return 0;
 }
 
-int default_trunc_callback(const char *){
+int default_trunc_callback(const char *path){
     return 0;
 }
 
-int default_read_callback(const char *, char *, size_t, off_t){
+int default_read_callback(const char *path, char *buf, size_t size, off_t offset){
     return 0;
 }
 
-int default_write_callback(const char *, const char *, size_t, off_t){
+int default_write_callback(const char *path, const char *buf, size_t size, off_t offset){
     return 0;
 }
 
-int version_size(const char *){
+int version_size(const char *path){
     return 40;
 }
 
 int version_read(const char * file, char * buffer, size_t size, off_t offset)
 {
-    char res[100] = {'\0'};
+    char res[41] = {'\0'};
     int l = 40;
 
     if (offset != 0)
         return 0;
 
-    strcpy(res, arduino->version().c_str());
-    memcpy(buffer, res, l+1);
+    HAL_READ(&arduino, version, res);
+    l = min(((int)size), l);
+    memcpy(buffer, res, l);
     return l;
 }
 
-int open_write(const char * file, const char * buffer, size_t size, off_t offset){
-    if(buffer[0] == '1')
-        arduino->on();
+int open_write(const char * file, const char * buffer, size_t size, off_t offset)
+{
+    if (buffer[0] == '1')
+        HAL_on(&arduino);
     else
-        arduino->off();
-
+        HAL_off(&arduino);
     return size;
 }
 
-hal_file all_paths[] = {
+halfs_file all_paths[] = {
     {
         .name = "/version",
         .mode = 0444,
@@ -73,22 +81,14 @@ hal_file all_paths[] = {
         .write_callback = open_write
     }
 };
-const size_t paths_size =  sizeof(all_paths)/sizeof(struct hal_file);
 
-static inline bool streq(const char *s1, const char *s2)
-{
-    while (*s1 && *s2 && *s1 == *s2){
-        s1++;
-        s2++;
-    }
-    return *s1 == *s2;
-}
+const size_t N_PATHS = sizeof(all_paths)/sizeof(struct halfs_file);
 
-static int hal_trunc(const char *path, off_t size){
+static int halfs_trunc(const char *path, off_t size){
     return 0;
 }
 
-static int hal_getattr(const char *path, struct stat *stbuf)
+static int halfs_getattr(const char *path, struct stat *stbuf)
 {
     memset(stbuf, 0, sizeof(struct stat));
 
@@ -99,7 +99,7 @@ static int hal_getattr(const char *path, struct stat *stbuf)
         stbuf->st_gid = getgid();
         return 0;
     }
-    for(int i = 0; i < paths_size; i++){
+    for (size_t i = 0; i < N_PATHS; i++){
         if(streq(path, all_paths[i].name)){
             stbuf->st_mode = S_IFREG | all_paths[i].mode;
             stbuf->st_nlink = 1;
@@ -112,26 +112,26 @@ static int hal_getattr(const char *path, struct stat *stbuf)
     return -ENOENT;
 }
 
-void * hal_init(struct fuse_conn_info *conn){
-    arduino = new Ambianceduino("/dev/tty.usbmodemfd111");
+void * halfs_init(struct fuse_conn_info *conn){
+    HAL_init(&arduino, "/dev/ttyACM0", 115200);
+    HAL_start(&arduino);
+    return NULL;
 }
 
-static int hal_open(const char *path, struct fuse_file_info *fi)
+static int halfs_open(const char *path, struct fuse_file_info *fi)
 {
-    for(int i = 0; i < paths_size; i++){
+    for (size_t i = 0; i < N_PATHS; i++){
         if(streq(path, all_paths[i].name)){
             return 0;
         }
     }
     return -ENOENT;
-
-    //TODO check flags
 }
 
-static int hal_read(const char *path, char *buf, size_t size, off_t offset,
+static int halfs_read(const char *path, char *buf, size_t size, off_t offset,
               struct fuse_file_info *fi)
 {
-    for(int i = 0; i < paths_size; i++){
+    for (size_t i = 0; i < N_PATHS; i++){
         if(streq(path, all_paths[i].name)){
             return all_paths[i].read_callback(path, buf, size, offset);
         }
@@ -140,10 +140,10 @@ static int hal_read(const char *path, char *buf, size_t size, off_t offset,
 }
 
 
-static int hal_write(const char *path, const char *buf, size_t size, off_t offset,
+static int halfs_write(const char *path, const char *buf, size_t size, off_t offset,
         struct fuse_file_info *fi) {
 
-    for(int i = 0; i < paths_size; i++){
+    for (size_t i = 0; i < N_PATHS; i++){
         if(streq(path, all_paths[i].name)){
             return all_paths[i].write_callback(path, buf, size, offset);
         }
@@ -151,7 +151,7 @@ static int hal_write(const char *path, const char *buf, size_t size, off_t offse
     return -ENOENT;
 }
 
-static int hal_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+static int halfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
         off_t offset, struct fuse_file_info *fi) {
 
     if (strcmp(path, "/") != 0)
@@ -159,7 +159,7 @@ static int hal_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
-    for (size_t i=0; i<paths_size; i++){
+    for (size_t i=0; i<N_PATHS; i++){
         filler(buf, all_paths[i].name + 1, NULL, 0);
     }
 
@@ -167,21 +167,18 @@ static int hal_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 }
 
 static struct fuse_operations hal_ops = {
-    .getattr    = hal_getattr,
-    .readdir    = hal_readdir,
-    .open       = hal_open,
-    .read       = hal_read,
-    .write      = hal_write,
-    .truncate   = hal_trunc,
-    .init       = hal_init
+    .getattr    = halfs_getattr,
+    .readdir    = halfs_readdir,
+    .open       = halfs_open,
+    .read       = halfs_read,
+    .write      = halfs_write,
+    .truncate   = halfs_trunc,
+    .init       = halfs_init
 };
-
-
-
 
 int main(int argc, char *argv[])
 {
-    for(int i = 0; i < paths_size; i++){
+    for (size_t i = 0; i < N_PATHS; i++){
         if (all_paths[i].size_callback == NULL)
             all_paths[i].size_callback = default_size_callback;
         if (all_paths[i].trunc_callback == NULL)
