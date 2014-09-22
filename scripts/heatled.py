abf8263e8df5ec1b130df0fa7a8fe517a0bc2313
iTitou <moiandme@gmail.com>
2014-09-22 22:04:50 +0200

    [fix] Open file for writing

--- scripts/heatled.py
+++ scripts/heatled.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
 	while True:
 		capted_temp = float(open(HALFS_ROOT+"/sensors/temp_radiator").read().strip("\0"))
 		transformed_temp = int(255*capted_temp)
-		open(HALFS_ROOT+"/animations/heater/frames").write(chr(limit(transformed_temp, 0, 255)))
+		open(HALFS_ROOT+"/animations/heater/frames", "w").write(chr(limit(transformed_temp, 0, 255)))
 		sleep(1)
