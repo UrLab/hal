1b83280370f4c58ed110fa13d5f39e039f0599a7
Florentin Hennecker <florentin.hennecker@gmail.com>
2014-09-22 15:55:36 +0200

    [enh] epileptic mode when number of people changed

--- scripts/redled.py
+++ scripts/redled.py
@@ -3,6 +3,7 @@
 from urllib2 import urlopen
 from json import loads
 from config import HALFS_ROOT
+from time import sleep
 
 macs_json = urlopen("http://pamela.urlab.be/mac.json").read()
 macs = loads(macs_json)
@@ -10,6 +11,10 @@ macs = loads(macs_json)
 known_people = len(macs["color"])
 
 if __name__ == "__main__" :
+	# If number of people has changed, epileptic mode for 2 seconds
+	if (int(open(HALFS_ROOT+"/animations/red/fps", 'r').read())/10 != known_people or True):
+		open(HALFS_ROOT+"/animations/red/fps", 'w').write(str(500))
+		sleep(2)
 	open(HALFS_ROOT+"/animations/red/fps", 'w').write(str(10*known_people))
 
 
