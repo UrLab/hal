af5aabf9aa51efceba405ffc675ea1da4c974551
iTitou <moiandme@gmail.com>
2014-09-25 09:58:05 +0200

    [enh] Trigger fall should be triggered by many LOW states

--- arduino/lib/hal/hal.h
+++ arduino/lib/hal/hal.h
@@ -20,30 +20,33 @@ class Resource {
 class Trigger : public Resource {
     private:
         const int _active_state; 
-        int _count_state;
+        int _count_active, _count_inactive;
     public:
         explicit Trigger(const char *name, int pin, int active_state=HIGH) : 
-            Resource(name, pin), _active_state(active_state), _count_state(0)
+            Resource(name, pin), _active_state(active_state), _count_active(0), 
+            _count_inactive(0)
         {
             pinMode(pin, INPUT);
         }
-        bool isActive() const {return _count_state == TRIGGER_AFTER;}
+        bool isActive() const {return _count_active == TRIGGER_AFTER;}
         void check(){hit(digitalRead(pin()));}
         void hit(int state){
-            if (state == _active_state && _count_state < TRIGGER_AFTER){
-                _count_state ++;
-                if (_count_state == TRIGGER_AFTER){
+            if (state == _active_state && _count_active < TRIGGER_AFTER){
+                _count_active ++;
+                if (_count_active == TRIGGER_AFTER){
                     Serial.print("!");
                     Serial.print(id(), DEC);
                     Serial.println("1");
+                    _count_inactive = 0;
                 }
-            } else if (state != _active_state){
-                if (_count_state == TRIGGER_AFTER){
+            } else if (state != _active_state && _count_inactive < TRIGGER_AFTER){
+                _count_inactive ++;
+                if (_count_inactive == TRIGGER_AFTER){
                     Serial.print("!");
                     Serial.print(id(), DEC);
                     Serial.println("0");
+                    _count_active = 0;
                 }
-                _count_state = 0;
             }
         }
 };
