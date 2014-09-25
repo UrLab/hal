8d10e92366062b5c285af40cac4dfca58f98ee61
Titouan Christophe <titouanchristophe@gmail.com>
2014-09-25 14:54:50 +0200

    [fix] typos + minisleep between anim upload

--- scripts/openclose.py
+++ scripts/openclose.py
@@ -2,17 +2,20 @@ import hal
 from time import sleep
 
 def open_hs():
+    print "OPEN the hackerspace"
     for anim in ("red", "green", "blue"):
         prefix = "/animations/" + anim
         hal.set(prefix + "/loop", True)
         hal.set(prefix + "/play", True)
         hal.set(prefix + "/fps", 40)
         hal.write(prefix + "/frames", hal.sinusoid(250, 0, 200))
+        sleep(0.0001)
 
     hal.set("/switchs/power", True)
-    hal.set("/switchs/leds_stairs/", True)
+    hal.set("/switchs/leds_stairs", True)
 
 def close_hs():
+    print "CLOSE the hackerspace"
     for anim in ("red", "green", "blue", "heater", "door_green"):
         prefix = "/animations/" + anim
         hal.set(prefix + "/play", False)
@@ -23,6 +26,7 @@ def close_hs():
 
 
 for trigger_name, state in hal.events():
+    print trigger_name, state
     if trigger_name != 'knife_switch':
         continue
 
