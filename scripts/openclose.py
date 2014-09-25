7e560315155a83515d889d616abc63b32d20895b
Nikita Marchant <nikita.marchant@gmail.com>
2014-09-25 15:34:33 +0200

    Add a logger

--- scripts/openclose.py
+++ scripts/openclose.py
@@ -1,8 +1,17 @@
 import hal
 from time import sleep
-from logging import getLogger
+import logging
+import sys
 
-log = getLogger(__name__)
+log = logging.getLogger(__name__)
+
+log.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
 
 
 def open_hs():
