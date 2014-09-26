42b94e5671cc850bb2eb4fe1db17f45223000b21
Titouan Christophe <titouanchristophe@gmail.com>
2014-09-26 13:23:12 +0200

    [enh] Better heatled

--- scripts/heatled.py
+++ scripts/heatled.py
@@ -5,9 +5,14 @@ from time import sleep
 
 
 def main():
+    last_temp = -1
+    hal.play("heater")
+    hal.fps("heater", 42)
     while True:
         temp = hal.sensor("temp_radiator")
-        hal.upload("heater", hal.sinusoid(max_value=temp))
+        if last_temp != temp:
+            hal.upload("heater", hal.sinusoid(val_max=temp, n_frames=100))
+            last_temp = temp
         sleep(10)
 
 
