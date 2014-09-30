bf9e8874ce0e1ab77655366f603bfd48870171d4
iTitou <moiandme@gmail.com>
2014-09-30 20:37:54 +0200

    [add] Ring buzzer when someone press the bell button

---
+++ scripts/bell.py
@@ -0,0 +1,24 @@
+import hal
+from time import sleep
+
+FUNKYTOWN = [
+    49, 49, 49, 0, 49, 49, 49, 0, 44, 44, 44, 0, 49, 49, 49, 49, 49, 49, 49, 0,
+    37, 37, 37, 37, 37, 37, 37, 0, 37, 37, 37, 0, 49, 49, 49, 0, 66, 66, 66, 0,
+    62, 62, 62, 0, 49, 49, 49, 49, 49, 49, 49, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0
+]
+
+def main():
+    funkytown = [FUNKYTOWN[i]*(1+(i%2)) for i in range(len(FUNKYTOWN))]
+    hal.stop('buzzer')
+    hal.upload('buzzer', funkytown)
+    hal.one_shot('buzzer')
+    while True:
+        hal.waitFor('bell')
+        hal.upload('bell_eyes', hal.sinusoid(n_frames=20, val_max=255))
+        hal.play('buzzer')
+        sleep(2)
+        hal.upload('bell_eyes', [255])
+
+if __name__ == "__main__":
+    main()
