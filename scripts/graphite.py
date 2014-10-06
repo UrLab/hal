4199886447a8c5052a69fd00dcca107c04284464
Nikita Marchant <nikita.marchant@gmail.com>
2014-10-06 16:40:06 +0200

    Change graphite delay to 20 sec

--- scripts/graphite.py
+++ scripts/graphite.py
@@ -3,12 +3,14 @@ from config import SENSORS_GRAPHITE, get_hal
 
 hal = get_hal()
 logger = hal.getLogger(__name__)
-server = GraphiteFeeder(SENSORS_GRAPHITE, prefix="hal", delay=60)
+server = GraphiteFeeder(SENSORS_GRAPHITE, prefix="hal", delay=20)
+
 
 @server.metric(multiple=True)
 def sensors():
     logger.info("Retrieve sensors...")
     return hal.sensors()
+
 
 def main():
     server.feed()
