332e9e4785ce0df149e17ad94a23da7d26fee452
iTitou <moiandme@gmail.com>
2015-02-17 10:58:01 +0100

    [add] Event to LechBot: someone inside

--- scripts/door_flash.py
+++ scripts/door_flash.py
@@ -13,6 +13,7 @@ def main():
             hal.play('door_green')
             logger.info('flash')
             internet.events.send("passage", ["passage"])
+            internet.lechbot_event('passage')
 
 if __name__ == "__main__":
     main()
